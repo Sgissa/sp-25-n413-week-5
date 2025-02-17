61b63bf106028c4e011f608ebc72597a93f9f9be
Sgissa <salmanegarba721@gmail.com>
2025-02-17 18:03:31 -0500

    Main-nav error

--- wp-content/themes/block-music-academy/function.php
+++ wp-content/themes/block-music-academy/function.php
@@ -1,1 +1,14 @@
 <?php
+function bma_custom_blocks_init()
+{
+    register_block_type(__DIR__ . "/build/main-nav");
+}
+
+add_action("init", "bma_custom_blocks_init");
+
+function music_school_scripts()
+{
+    wp_enqueue_style("school-style", get_stylesheet_uri());
+}
+
+add_action("wp_enqueue_scripts", "music_school_scripts");
